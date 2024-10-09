d90f0d8052682e4c4dcd5a3d3577313a866ce0e4
Nariryn <122385906+Nariryn@users.noreply.github.com>
2024-10-10 01:04:29 +0700

    practice

--- chap_9_sort/1_bubble_sort_recur.py
+++ chap_9_sort/1_bubble_sort_recur.py
@@ -5,18 +5,36 @@ def bubble_sort_recur(a, i, j, n):
     if i == n:
         return 
     if a[i] < a[j]:
+        #min to max
         temp = a[j]
-        # print(f"a j {temp}")
         a[j] = a[i]
-        # print(f"a j = i {temp}")
         a[i] = temp
-        # print(f"a i {temp}")
         bubble_sort_recur(a, i, j+1, n)
     else:
         bubble_sort_recur(a, i, j+1, n)
     return a
 
 a_inp = list(map(int, input("Enter Input : ").split()))
-# print(a_inp)
 a = bubble_sort_recur(a_inp, 0, 0, len(a_inp))
 print(a)
+
+'''
+เขียน function bubble sort เพื่อเรียงข้อมูลใน list จากน้อยไปมาก โดยใช้ recursive
+
+***ห้ามใช้ คำสั่งloopต่างๆ เช่น for ,while หรือ Built-in Function ที่เกี่ยวกับ Sort เช่น .sort***
+
+*** ยกเว้นให้ใช้  for ได้แค่ขั้นตอนรับ input เท่านั้น ***
+
+testcase 1
+Enter Input : 4 3 2 1
+[1, 2, 3, 4]
+
+testcase 2
+Enter Input : 3 2 1 5 6 7
+[1, 2, 3, 5, 6, 7]
+
+testcase 3
+Enter Input : 1 2 3 4 5
+[1, 2, 3, 4, 5]
+
+'''
---
+++ practice/9_sort/steaight_selecttion_sort.py
@@ -0,0 +1,23 @@
+def find_max_index(arr, start, max_index):
+    if start < 0:
+        return max_index    
+    if arr[start] > arr[max_index]:
+        max_index = start   
+    return find_max_index(arr, start -1, max_index)
+
+
+def Selection(index,lst):
+    if index <=  0:
+        print(lst)
+        return 
+    
+    max_ =find_max_index(lst,index,index)
+    lst[index] ,lst[max_]  = lst[max_],lst[index] 
+    if not index == max_:
+        print(f"swap {lst[max_]} <-> {lst[index]} : {lst}")
+
+    Selection(index-1,lst)
+
+inp = list(map(int,input("Enter Input : ").split(" ")))
+
+Selection(len(inp)-1,inp)
